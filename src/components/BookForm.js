0bea39c375b8080d2cfb266b8fc60b117e76657d
Leah Garrett <leah_garrett@hotmail.com>
2019-07-03 23:26:50 +1000

    added display of messages for form validation errors

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -9,7 +9,12 @@ class BookForm extends Component {
             author: props.author || '',
             genre: props.genre || '',
             price: props.price || 0,
-            id: props.id || 0
+            id: props.id || 0,
+            formErrorMessage: '', 
+            titleValid: true,
+            authorValid: true,
+            genreValid: true,
+            priceValid: true,
         };
       }
 
@@ -19,7 +24,11 @@ class BookForm extends Component {
     if (regex.test(this.state.price)){
       return true;
     }
-    console.log('Invalid number - requires two characters after decimal')
+    this.setState({ formErrorMessage: 'Invalid number - require valid number with two decimal places',            
+      titleValid: true,
+      authorValid: true,
+      genreValid: true,
+      priceValid: false });
     return false;
   }
 
@@ -27,11 +36,14 @@ class BookForm extends Component {
     if ( this.state.title.length > 0 
       && this.state.author.length > 0  
       && this.state.genre.length > 0 
-      && (this.state.price.length > 0)
-    ) {
+      && this.state.price.length > 0) {
       return true;
     }
-    console.log('Missing field')
+    this.setState({ formErrorMessage: 'All fields required',            
+      titleValid: this.state.title.length,
+      authorValid: this.state.author.length,
+      genreValid: this.state.genre.length,
+      priceValid: this.state.price.length });
     return false;
   }
 
@@ -62,6 +74,7 @@ class BookForm extends Component {
       return (
         <div className="book-form">
           <h1>Book Form</h1>
+          {this.state.formErrorMessage.length > 0 && <h3 className="error">{this.state.formErrorMessage}</h3>}
           <form>
             <label htmlFor="title">Title</label>
               <input 
@@ -69,6 +82,7 @@ class BookForm extends Component {
                 type="text" 
                 id="title" 
                 placeholder="title" 
+                className={!this.state.titleValid ? "invalid" : undefined}
                 value={this.state.title} 
               />
               <label htmlFor="author">Author</label>
@@ -77,6 +91,8 @@ class BookForm extends Component {
                 type="text" 
                 id="author" 
                 placeholder="author" 
+                className={!this.state.authorValid ? "invalid" : undefined}
+
                 value={this.state.author} 
               />
               <label htmlFor="genre">Genre</label>
@@ -85,6 +101,7 @@ class BookForm extends Component {
                 type="text" 
                 id="genre" 
                 placeholder="genre" 
+                className={!this.state.genreValid ? "invalid" : undefined}
                 value={this.state.genre} 
               />
               <label htmlFor="price">Price</label>
@@ -93,6 +110,7 @@ class BookForm extends Component {
                 type="number" 
                 id="price" 
                 placeholder="price" 
+                className={!this.state.priceValid ? "invalid" : undefined}
                 value={this.state.price} 
               />
 
